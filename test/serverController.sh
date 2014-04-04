36be2fe696ed7e2696fe536c3eb18cce3d33fae3
Gadzhi Gadzhiev <gadzhi@gadzhiev.com>
2014-04-04 20:24:45 +0400

    Add update command to serverController mock

--- test/serverController.sh
+++ test/serverController.sh
@@ -4,6 +4,9 @@ case "$1" in
   start )
     sleep 2
     echo "Running $2 $3" > test/current_status;;
+  update )
+    sleep 2
+    echo "Running $2 $3" > test/current_status;;
   stop )
     sleep 2
     echo "Halted" > test/current_status;;
